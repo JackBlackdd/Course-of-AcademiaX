3b9a9c130bb876c67a80d3906b4ff90a05a8bb30
Jason Barcelo <jasonfernely@hotmail.com>
2022-03-17 19:39:44 -0500

    JS

--- JS/08 Módulos/Importar.js
+++ JS/08 Módulos/Importar.js
@@ -1,18 +1,17 @@
-// import { sumar, PI, Servicio } from './export.js';
+//? ------------------------------------------------------------
+import { sumar, PI, Servicio } from './Exportar.js';
 
-// let resultado = sumar(1,2);
-// console.log(resultado);
-// console.log(PI);
-// const servicio = new Servicio();
-// console.log(servicio.restarTres(PI));
+let resultado = sumar(1, 2);
+console.log(resultado);
+console.log(PI);
+const servicio = new Servicio();
+console.log(servicio.restarTres(PI));
+//? ------------------------------------------------------------
+import cualquierNombre from './Exportar.js';
 
-// import cualquierNombre from './export.js';
+console.log(new cualquierNombre().restarTres(4));
+//? ------------------------------------------------------------
+import * as todo from './Exportar.js';
 
-// console.log(new cualquierNombre().restarTres(4));
-
-import * as todo from './export.js';
-
-console.log(todo.sumar(4,5));
+console.log(todo.sumar(4, 5));
 console.log(todo.Servicio);
-
-
