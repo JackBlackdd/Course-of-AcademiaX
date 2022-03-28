e778ad749b25fb1916d446409451b6ce41df203c
Jason Barcelo <jasonfernely@hotmail.com>
2022-03-28 03:07:19 -0500

    Website

---
+++ docs/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    entry: ['./index.js'],
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, "dist"),
+        assetModuleFilename: 'assets/[hash][ext][query]'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "[name].[hash].css" }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({ template: './index.html' })
+    ],
+    module: {
+        rules: [{
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.css$/i,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+                    },
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                type: 'asset/resource'
+            }
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ]
+    },
+
+};
