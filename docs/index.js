fdee491c6bb9ef57fb3a4937094f8fc78908ab96
Jason Barcelo <jasonfernely@hotmail.com>
2022-03-30 09:01:36 -0500

    Website

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,4 @@
 import './assets/js/fixed.js';
-import './assets/js/hamburguesa.js';
+import './assets/js/Hamburguesa.js';
 import './node_modules/eins-modal/dist/js/eins-modal.min.js';
 import './assets/css/index.css';
